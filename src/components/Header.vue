68ec83320426d8210590f86c0d0b4cbbfbd7e240
byan799 <bo.yan799@gmail.com>
2018-12-30 15:29:58 +0800

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont icon-back"></div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont icon-search"></span>
+            <span class="text">输入城市/景点/游玩主题</span>
+        </div>
+        <div class="header-right">
+            <span class="text">城市</span>
+            <span class="iconfont icon-arrow"></span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl'
+    .header
+        background: $bgColor
+        height: .86rem
+        line-height: .86rem
+        display: flex
+        .header-left
+            width: .64rem
+            .icon-back
+                text-align: center
+        .header-input
+            flex: 1
+            height: .64rem
+            line-height: .64rem
+            margin-top: .12rem
+            background: #fff
+            border-radius: .1rem
+            padding-left: .2rem
+        .header-right
+            width: 1.24rem
+            text-align: center
+            .icon-arrow 
+                font-size: .25rem
+</style>
+
+
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="home">
+        <home-header></home-header>
+    </div>
+</template>
+<script>
+import HomeHeader from '@/components/Header'
+export default {
+    components: {
+        HomeHeader
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    
+</style>
+
+
